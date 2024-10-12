76039ff728acf739c281ff752be2849723aadcb7
webb-orbit <webbenemies@gmail.com>
2024-10-12 10:21:04 +0530

    hello motto

--- src/app.js
+++ src/app.js
@@ -9,26 +9,7 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json({limit:"20kb"}));
 app.use(express.urlencoded({extended: true}));
-const allowedOrigins = ['http://localhost:5173', 'https://rerander.vercel.app'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("origin->>>>>> ",origin);
-    
-    // Allow requests with no origin (like mobile apps or Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
-  credentials: true, 
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 import clientrouter from "./router/client.router.js";
